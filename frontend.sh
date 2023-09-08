69cf0b0fa9bdce82611e1e078b86d844d0090602
krishnareddyB7 <saikrishnareddyb7@gmail.com>
2023-09-09 00:45:30 +0530

    expense-1

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,12 @@
 dnf install nginx -y
 
 cp expense.conf /etc/nginx/default.d/expense.conf
-systemctl enable nginx
+systemctl enable nginx >/tmp/sai
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tmp/sai
 
 
 
